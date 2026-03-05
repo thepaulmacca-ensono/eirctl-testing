18f32d8025eb8c0bd9694f0eb46a0117389b122a
Paul McDonald <paul.mcdonald@ensono.com>
2026-03-05 12:15:37 +0000

    Update tasks and fmt

--- src/management_landing_zone/deploy/terraform/main.tf
+++ src/management_landing_zone/deploy/terraform/main.tf
@@ -32,11 +32,11 @@ resource "azurerm_log_analytics_workspace" "management" {
 
 # Storage Account - deliberately insecure for checkov testing
 resource "azurerm_storage_account" "management" {
-  name                          = "stmanagement"
-  location                      = azurerm_resource_group.management.location
-  resource_group_name           = azurerm_resource_group.management.name
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
-  https_traffic_only_enabled    = false
+  name                            = "stmanagement"
+  location                        = azurerm_resource_group.management.location
+  resource_group_name             = azurerm_resource_group.management.name
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  https_traffic_only_enabled      = false
   allow_nested_items_to_be_public = true
 }
