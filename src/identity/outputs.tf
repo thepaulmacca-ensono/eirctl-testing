3930fa02558f9cb393ddc1a79b9df0febafa0f89
Paul McDonald <paul.mcdonald@ensono.com>
2026-03-26 13:25:59 +0000

    Remove identity module

--- src/identity/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.identity.name
-  description = "Name of the resource group created for the Identity landing zone"
-}
--- src/identity/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group to create"
-}
